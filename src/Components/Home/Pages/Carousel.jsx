843e0c6d46e4e9b027b4cdae822e2a2c10f25c68
Tarek Siddique Nabil <86683713+Tarek-Siddique-Nabil@users.noreply.github.com>
2023-09-04 02:51:19 +0600

    carousel fixed

--- src/Components/Home/Pages/Carousel.jsx
+++ src/Components/Home/Pages/Carousel.jsx
@@ -87,9 +87,10 @@ const Carousel = () => {
 
   return (
     <section className="lg:px-40 md:px-10 px-5 md:py-10 py-4">
-      <div className="h-[420px] bg-black rounded-xl flex gap-5 items-center relative overflow-hidden">
-        <div className="">
-          <Ticker duration={100}>
+      <div className="h-[215px] lg:h-[420px] md:h-[208px] bg-black rounded-xl flex gap-5 items-center relative overflow-hidden">
+        {/* marque1 */}
+        <div className="marquee1">
+          <div className="marquee__group">
             {datas?.map((data, index) => (
               <div
                 key={index}
@@ -105,7 +106,11 @@ const Carousel = () => {
                   fill="currentColor"
                   className={`w-8 h-8 ${index % 2 !== 0 && "text-violet-500"}`}
                 >
-                  {/* SVG path here */}
+                  <path
+                    fillRule="evenodd"
+                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                    clipRule="evenodd"
+                  />
                 </svg>
 
                 <p className="text-3xl text-gray-200 font-Manrope">
@@ -116,7 +121,106 @@ const Carousel = () => {
                 </p>
               </div>
             ))}
-          </Ticker>
+          </div>
+          <div className="marquee__group" aria-hidden="true">
+            {datas?.map((data, index) => (
+              <div
+                key={index}
+                className={`w-[290px] mx-5 flex flex-col gap-3 rounded-lg  ${
+                  index % 2 === 0
+                    ? "bg-violet-500 "
+                    : "bg-transparent border border-slate-500"
+                } p-4`}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className={`w-8 h-8 ${index % 2 !== 0 && "text-violet-500"}`}
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+
+                <p className="text-3xl text-gray-200 font-Manrope">
+                  {data.title}
+                </p>
+                <p className="font-Manrope text-base text-white ">
+                  {data.short_description}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* marquee 2 */}
+        <div className="marquee2 hidden lg:flex">
+          <div className="marquee__group2">
+            {datas?.map((data, index) => (
+              <div
+                key={index}
+                className={`w-[290px] mx-5 flex flex-col gap-3 rounded-lg  ${
+                  index % 2 === 0
+                    ? "bg-violet-500 "
+                    : "bg-transparent border border-slate-500"
+                } p-4`}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className={`w-8 h-8 ${index % 2 !== 0 && "text-violet-500"}`}
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+
+                <p className="text-3xl text-gray-200 font-Manrope">
+                  {data.title}
+                </p>
+                <p className="font-Manrope text-base text-white ">
+                  {data.short_description}
+                </p>
+              </div>
+            ))}
+          </div>
+          <div className="marquee__group2" aria-hidden="true">
+            {datas?.map((data, index) => (
+              <div
+                key={index}
+                className={`w-[290px] mx-5 flex flex-col gap-3 rounded-lg  ${
+                  index % 2 === 0
+                    ? "bg-violet-500 "
+                    : "bg-transparent border border-slate-500"
+                } p-4`}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className={`w-8 h-8 ${index % 2 !== 0 && "text-violet-500"}`}
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+
+                <p className="text-3xl text-gray-200 font-Manrope">
+                  {data.title}
+                </p>
+                <p className="font-Manrope text-base text-white ">
+                  {data.short_description}
+                </p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
