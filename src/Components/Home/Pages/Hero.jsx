871dd72aab42dee183cb1a2d9719672a99b06d23
Tarek Siddique Nabil <86683713+Tarek-Siddique-Nabil@users.noreply.github.com>
2023-09-13 05:53:38 +0600

    Update Hero.jsx

--- src/Components/Home/Pages/Hero.jsx
+++ src/Components/Home/Pages/Hero.jsx
@@ -81,8 +81,7 @@ const Hero = () => {
           className="-z-50 flex flex-col gap-5"
         >
           <p className="text-[22px] md:text-[26px] font-Manrope font-bold">
-            {" "}
-            AI Transformation Services to:
+            AI Transformation, IT Services and Management Consulting to:
           </p>
           <ul className="text-[22px] leading-10 font-Manrope list-inside list-disc">
             <li>Increase Productivity by 10x</li>
