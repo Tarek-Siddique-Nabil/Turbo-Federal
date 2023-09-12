06f8eaaa79677154a2c3cdb8eb9f330e9be5ea03
Tarek Siddique Nabil <86683713+Tarek-Siddique-Nabil@users.noreply.github.com>
2023-09-12 19:34:43 +0600

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React,{ useEffect} from "react";
 import Hero from "./Pages/Hero";
 import Carousel from "./Pages/Carousel";
 import Work from "./Pages/Work";
 import Team from "./Pages/Team";
 
 const Home = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scrolls to the top of the page when the component mounts
+  }, []);
   return (
     <div className="overflow-hidden">
       <Hero />
